ada16ca12a17443c2e700ff86b7114e6147f2379
jason-aultman <jason.aultman@gmail.com>
2020-07-09 21:07:39 -0400

    add new homework

--- HomeWork07092020/Program.cs
+++ HomeWork07092020/Program.cs
@@ -6,7 +6,27 @@ namespace HomeWork07092020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your age ");
+            bool success = int.TryParse(Console.ReadLine(), out int userAge);
+            if (!success)
+            {
+                Console.WriteLine("I don't think that was a number");
+                return;
+
+            }
+            if (success && userAge>16)
+            {
+                Console.WriteLine("You are old enough to drive alone");
+            } 
+            else if (success && userAge >=14 && userAge <16)
+            {
+                Console.WriteLine("You can drive with an adult");
+            }
+            else
+            {
+                Console.WriteLine("You probably shouldn't be driving");
+            }
         }
     }
+
 }
